b047e7b653b06d00ed06f8a027b1b663d12f385f
JeremyC <jeremy.cranfill@gmail.com>
2017-01-24 21:45:40 -0600

    added lucky number and refresh button

---
+++ main.py
@@ -0,0 +1,62 @@
+    #!/usr/bin/env python
+    #
+    # Copyright 2007 Google Inc.
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #     http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+    #
+import webapp2
+import random
+
+def getRandomFortune():
+    #make list of possible fortunes
+    fortunes=[
+    "You are a cool person",
+    "Life aint great.",
+    "Dont leave the house today",
+    "Look to la luna",
+    "this is dumb"]
+    #randomly select a fortunes
+    index = random.choice(fortunes)
+
+
+
+
+    #randomlyselect a fortunes
+
+
+    return index
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        header ="<h1>Fortune Cookie</h1>"
+
+        fortune ="<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence ="Your fortune: " +fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+        lucky_number = "<strong>" + str(random.randint(1,200)) + "</strong>"
+        numberSentence = "Your lucky number: " + lucky_number
+        numberParagraph = "<p>" + numberSentence + "</p>"
+        cookie_again_button = "<a href='.''><button>Another cookie please</button></a>"
+        content = header + fortune_paragraph + numberParagraph +cookie_again_button
+
+        self.response.write(content)
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Thanks for trying to login!")
+
+
+
+app = webapp2.WSGIApplication([ ('/', MainHandler), ('/login',LoginHandler)
+    ]
+
+, debug=True)
